c4554690301bc97704b08d71eb8db73742b2ddf3
Omkulthoom <sheng.na.u@hotmail.com>
2020-05-04 06:57:24 +0300

    Initial Commit

---
+++ work-3/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  work-3
+//
+//  Created by cheenlee on 5/4/20.
+//  Copyright © 2020 cheenlee. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
